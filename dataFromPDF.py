6b6f02035b11c3768333c44238e5de425466eae3
Science BD <kn4ihp@Sciences-MacBook-Pro.local>
2024-08-29 12:02:32 -0400

    second

---
+++ dataFromPDF.py
@@ -0,0 +1,18 @@
+import re
+import os
+import pdfminer
+from pdfminer.high_level import extract_text
+
+
+pdf = 'test1.pdf'
+
+
+text = extract_text(pdf)
+
+# feed text to ollama API
+
+
+# get the output from ollama API
+
+
+
